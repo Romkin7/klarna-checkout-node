902447eb930adae10c17588ef9c5aa531e5c2f4c
Roman Tuomisto <romantuomisto@gmail.com>
2022-09-18 18:38:51 +0300

    adds jest env vars

--- .jest/setEnvVars.example.js
+++
@@ -1,4 +0,0 @@
-process.env.NODE_ENV = 'production';
-process.env.EID = 'your klarna test env eid';
-process.env.KLARNA_SECRET = 'your klarna test env klarna secret key';
-process.env.PATH = 'https://www.rollingrecords.fi';
--- .jest/setEnvVars.js
+++ .jest/setEnvVars.js
@@ -1,4 +1,4 @@
 process.env.NODE_ENV = 'development';
-process.env.EID = 'PK33382_844178e95f74';
-process.env.KLARNA_SECRET = 'Wpgeh1ResJ6NCGp7';
+process.env.EID = 'PK79678_321178p9589';
+process.env.KLARNA_SECRET = 'Wucwz1BeyJ1NCGp5';
 process.env.PATH = 'https://www.rollingrecords.fi';
