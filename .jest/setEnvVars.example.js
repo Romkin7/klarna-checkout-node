81c137aec838424e5cb74c461e024eb544c8b110
Roman Tuomisto <romantuomisto@gmail.com>
2022-09-18 11:12:28 +0300

    adds eslint and jest configs

---
+++ .jest/setEnvVars.example.js
@@ -0,0 +1,4 @@
+process.env.NODE_ENV = 'production';
+process.env.EID = 'your klarna test env eid';
+process.env.KLARNA_SECRET = 'your klarna test env klarna secret key';
+process.env.PATH = 'https://www.rollingrecords.fi';
---
+++ .jest/setEnvVars.js
@@ -0,0 +1,4 @@
+process.env.NODE_ENV = 'development';
+process.env.EID = 'PK33382_844178e95f74';
+process.env.KLARNA_SECRET = 'Wpgeh1ResJ6NCGp7';
+process.env.PATH = 'https://www.rollingrecords.fi';
