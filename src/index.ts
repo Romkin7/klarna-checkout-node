59e3281540ad66b0459a42bb76c7991d221922cf
Roman Tuomisto <romantuomisto@gmail.com>
2022-09-18 21:38:43 +0300

    update version in index

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  *
  * @author Roman Tuomisto
  * Support email: devdesignrt@gmail.com
- * @version 1.0.1
+ * @version 1.0.2
  */
 import { initialize } from './initialize';
 import { createOrder } from './createOrder';
